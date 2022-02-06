6fbefde92240c979bbbe598807a9ae4718b50dbe
Ben01cephas <mahoungoucephas@gmail.com>
2022-02-06 23:42:40 +0000

    version finale

--- produit_app/produit_app/settings.py
+++ produit_app/produit_app/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$o6x0gg9(@)sqc4dg1^4rynw-0z+rmxe3c#$6q58i0j%r_^3v2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- produit_app/produit_app/urls.py
+++ produit_app/produit_app/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tpproduit/', include('tpproduit.urls')),
+    path('', include('tpproduit.urls')),
     path('admin/', admin.site.urls),
 ]
